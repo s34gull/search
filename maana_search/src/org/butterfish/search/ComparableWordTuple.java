ba9b05f381bb768395a3c671044864995486ab75
Jonathan Edwards <s34gull@gmail.com>
2015-08-17 16:23:59 -0700

    development checkpoint

--- maana_search/src/org/butterfish/search/ComparableWordTuple.java
+++ maana_search/src/org/butterfish/search/ComparableWordTuple.java
@@ -3,17 +3,30 @@ package org.butterfish.search;
 public class ComparableWordTuple implements Comparable<ComparableWordTuple> {
 
 	private String documentName;
-	
-	private int count;
-	
-	private String word; 
-	
-	public ComparableWordTuple(String documentName, String word, int count) {
+
+	private long count;
+
+	private String word;
+
+	public ComparableWordTuple(String documentName, String word, long count) {
+		if (documentName == null || word == null || count < 1 || "".equals(documentName) || "".equals(word)) {
+			StringBuilder strbld = new StringBuilder();
+			if (documentName == null || "".equals(documentName)) {
+				strbld.append("Parameter 'documentName' cannot be null or empty. ");
+			}
+			if (word == null || "".equals(word)) {
+				strbld.append("Parameter 'word' cannot be null or empty. ");
+			}
+			if (count < 1) {
+				strbld.append("Parameter 'count' cannot be less than '1'. ");
+			}
+			throw new IllegalArgumentException(strbld.toString());
+		}
 		this.documentName = documentName;
 		this.word = word;
 		this.count = count;
 	}
-	
+
 	public String getDocumentName() {
 		return documentName;
 	}
@@ -22,15 +35,32 @@ public class ComparableWordTuple implements Comparable<ComparableWordTuple> {
 		return Math.log(count);
 	}
 
-	public int getCount() {
+	public long getCount() {
 		return count;
 	}
-	
+
 	public String getWord() {
 		return word;
 	}
-	
-	public int compareTo(ComparableWordTuple t) {
-		return (int)(this.getScore() - t.getScore())*1000;
+
+	public int compareTo(ComparableWordTuple cwt) {
+		int retval = 1;
+		
+		if (cwt != null) {
+			retval = (int) (this.getScore() - cwt.getScore()) * 1000;
+		}
+		
+		return retval;
+	}
+
+	public boolean equals(Object obj) {
+		boolean isEqual = false;
+
+		if (obj != null && obj.getClass() == getClass()) {
+			final ComparableWordTuple cwt = (ComparableWordTuple) obj;
+			isEqual = (cwt.getDocumentName() == documentName && cwt.getWord() == word);
+		}
+		
+		return isEqual;
 	}
 }
--- maana_search/src/org/butterfish/search/HashSearch.java
+++ maana_search/src/org/butterfish/search/HashSearch.java
@@ -1,15 +1,40 @@
 package org.butterfish.search;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.maana.search.Search;
 import com.maana.search.SearchResult;
 
-public class HashSearch implements Search { 
+import uk.ac.cam.ha293.tweetlabel.util.Stopwords;
+
+public class HashSearch implements Search {
+
+	private final ConcurrentHashMap<String, ConcurrentSkipListSet<ComparableWordTuple>> searchIndex;
+
+	public HashSearch() {
+		searchIndex = new ConcurrentHashMap<String, ConcurrentSkipListSet<ComparableWordTuple>>();
+	}
 
 	@Override
-	public void indexDocument(String documentName, String doc) {
-		// TODO Auto-generated method stub
+	public void indexDocument(final String documentName, final String doc) {
+		List<String> cleansedDoc = Stopwords.removeStemmedStopWords(doc);
+
+		Map<String, Long> collection = cleansedDoc.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		
+		for(Entry<String,Long> entry : collection.entrySet()) {
+			String word = entry.getKey();
+			Long count = entry.getValue();
+			// try to get the Set mapped to the word and add this document
+			ComparableWordTuple cwt = new ComparableWordTuple(documentName, word, count);
+			// use locks to control creation of new entry in map
+		}
 
 	}
 
